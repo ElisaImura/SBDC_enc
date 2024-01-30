1c16e4b9d98d2c4c5d84ef5c7523123f843a08a3
ElisaImura <elisaimura45@gmail.com>
2024-01-30 17:06:10 -0300

    Vista de Login

---
+++ gestion_inv/resources/views/log/login.blade.php
@@ -0,0 +1,41 @@
+<head>
+    <title>Easy System</title>
+    @include('layouts.head')   
+</head> 
+
+<body>
+	<div id="main-container">
+		<div class="content">
+			<div class="container">
+				<div id="form" class="row justify-content-center">
+                    <div class="col-md-8">
+
+                        <div class="card">
+                            <div class="card-header">Login</div>
+                            
+                            <div class="card-body">
+                              <form method="POST" action="/clientes/create">
+                                    @csrf <!-- Campo CSRF -->
+                                    <div class="form-group">
+                                        <label for="usu_nombre">Usuario:</label>
+                                        <input type="text" id="usu_nombre" name="usu_nombre" class="form-control" placeholder="Ingrese su nombre de usuario">
+                                    </div>
+                                    
+                                    <div class="form-group">
+                                        <label for="usu_contra">Contraseña:</label>
+                                        <input type="password" id="usu_contra" name="usu_contra" class="form-control" placeholder="Ingrese su contraseña">
+                                    </div>
+                                    
+                                    <div class="form-group">
+                                        <button type="submit" class="btn btn-primary">Iniciar Sesión</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+			</div>
+		</div>
+	</div>
+</body>
