f88af0dd680bb936f162649384f27a95c9f4f2e2
ElisaImura <elisaimura45@gmail.com>
2024-01-30 19:07:29 -0300

    Problem solving

--- gestion_inv/app/Http/Controllers/UsuarioController.php
+++ gestion_inv/app/Http/Controllers/UsuarioController.php
@@ -19,7 +19,7 @@ class UsuarioController extends Controller
         return view('usuarios.formulario');
     }
 
-    public function registrarUsuario(Request $request)
+    public function create(Request $request)
     {
         // Validar los datos del formulario
         $request->validate([
@@ -43,5 +43,47 @@ class UsuarioController extends Controller
 
         // Redirigir a la página de inicio o a donde desees después del registro
         return redirect()->route('dashboard');
+    }
+
+    public function destroy($cli_id)
+    {
+        try {   
+            $cliente = Cliente::find($cli_id);
+
+            if (!$cliente) {
+                return redirect()->route('clientes.index')->with('error', 'Cliente no encontrado');
+            }
+
+            $cliente->delete();
+            return redirect()->route('clientes.index')->with('success', 'Cliente eliminado correctamente');
+        } catch (\Exception $e) {
+            return redirect()->route('clientes.index')->with('error', 'Cliente no se puede eliminar');
+        }
+    }
+
+    public function edit($cli_id)
+    {
+        $cliente = Cliente::find($cli_id);
+        return view('clientes.edit', compact('cliente'));
+    }
+
+    public function update(Request $request, $cli_id)
+    {
+        $cliente = Cliente::find($cli_id);
+        $cliente->cli_nombre = $request->input('cli_nombre');
+        $cliente->cli_apellido = $request->input('cli_apellido');
+        $cliente->cli_ruc = $request->input('cli_ruc');
+        $cliente->cli_direccion = $request->input('cli_direccion');
+        $cliente->cli_telefono = $request->input('cli_telefono');
+   
+
+        $cliente->save();
+        return redirect()->route('clientes.index')->with('success', 'Cliente actualizado correctamente');
+    }
+
+    public function show($cli_id)
+    {
+        $cliente = Cliente::with('cliente')->findOrFail($cli_id);
+        return view('clientes.show', compact('cliente'));
     }
 }
